4582d229ed1d2882b75d0f841111e8060b56fe03
Maryfel Mendoza <maryfelmendoza@hotmail.com>
2020-01-15 22:58:59 -0500

    indentation

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -1,16 +1,16 @@
 require_relative '../lib/player'
 
 RSpec.describe Player do
-    let(:player) { Player.new('player 1', 1) }
+  let(:player) { Player.new('player 1', 1) }
     
-    context '#initialize' do
-        it 'player object should be a Player' do
-            expect(player).to be_a Player
-        end
-        
-        it 'Player instance should receive two parameters' do
-            allow_any_instance_of(Player).to receive(:name)
-            allow_any_instance_of(Player).to receive(:player_number)
-        end
+  context '#initialize' do
+    it 'player object should be a Player' do
+      expect(player).to be_a Player
     end
+        
+    it 'Player instance should receive two parameters' do
+      allow_any_instance_of(Player).to receive(:name)
+      allow_any_instance_of(Player).to receive(:player_number)
+    end
+  end
 end
