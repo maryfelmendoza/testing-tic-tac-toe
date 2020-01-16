b057f794287c04e012e5083c1525c8a7aa60c5af
Maryfel Mendoza <maryfelmendoza@hotmail.com>
2020-01-15 22:53:26 -0500

    indentation

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -5,19 +5,19 @@ RSpec.describe Board do
 
   context '#set_index' do
     it 'return true if the value is positioned on the board player 1' do
-        (1...9).each do |index|
-          expect(board.set_index(index, 1)).to eql(true)
-        end
+      (1...9).each do |index|
+        expect(board.set_index(index, 1)).to eql(true)
+      end
     end
 
     it 'return true if the value is positioned on the board player 2' do
-        (1...9).each do |index|
-          expect(board.set_index(index, 2)).to eql(true)
-        end
+      (1...9).each do |index|
+        expect(board.set_index(index, 2)).to eql(true)
+      end
     end
 
     it 'return false if is not posible to set' do
-        expect(board.set_index(10, 1)).to eql(false)
+      expect(board.set_index(10, 1)).to eql(false)
     end
   end
 end
