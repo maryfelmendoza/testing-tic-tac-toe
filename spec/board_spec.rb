e36c5db6d4b0c7c7062c81c7180583713f28ffc1
Maryfel Mendoza <maryfelmendoza@hotmail.com>
2020-01-15 22:49:55 -0500

    indention

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -1,25 +1,23 @@
-# frozen_string_literal: true
-
 require_relative '../lib/board'
 
 RSpec.describe Board do
     subject(:board) { Board.new }
 
-    context '#set_index' do
-        it 'return true if the value is positioned on the board player 1' do
-            (1...9).each do |index|
-                expect(board.set_index(index, 1)).to eql(true)
-            end
+  context '#set_index' do
+    it 'return true if the value is positioned on the board player 1' do
+        (1...9).each do |index|
+          expect(board.set_index(index, 1)).to eql(true)
         end
+    end
 
-        it 'return true if the value is positioned on the board player 2' do
-            (1...9).each do |index|
-                expect(board.set_index(index, 2)).to eql(true)
-            end
+    it 'return true if the value is positioned on the board player 2' do
+        (1...9).each do |index|
+          expect(board.set_index(index, 2)).to eql(true)
         end
+    end
 
-        it 'return false if is not posible to set' do
-            expect(board.set_index(10, 1)).to eql(false)
+    it 'return false if is not posible to set' do
+        expect(board.set_index(10, 1)).to eql(false)
         end
     end
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 require_relative '../lib/game'
 
 RSpec.describe Game do
